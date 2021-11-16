b93193d6ae8b7c28d58a5088b48bf0e4f77c3730
Danila Dorozhkin <80399177+Tanukyes@users.noreply.github.com>
2021-11-16 11:13:58 +0300

    Update code

--- Cylinder.cpp
+++ Cylinder.cpp
@@ -2,36 +2,58 @@
 
 Cylinder::Cylinder()
 {
-	x = 0;
-	y = 0;
-	radius = 1;
-	height = 1;
+    x = 0;
+    y = 0;
+    radius = 1;
+    height = 1;
 }
 
-Cylinder::Cylinder(double _x, double _y,
-	double _radius, double _height)
+Cylinder::Cylinder(const double _x, const double _y,
+                   const double _radius, const double _height)
 {
-	x = _x;
-	y = _y;
-	radius = _radius;
-	height = _height;
+    x = _x;
+    y = _y;
+
+    setRadius(_radius);
+    setHeight(_height);
 }
 
 double Cylinder::get_square()
 {
-	return 2 * M_PI * radius * height +
-		2 * M_PI * radius * radius;
+    return 2 * M_PI * radius * height +
+           2 * M_PI * radius * radius;
 }
 
 string Cylinder::to_string()
 {
-	stringstream ss;
-	ss << "Цилиндр с центром в точке (" << x << ", " << y <<
-		") и радиусом " << radius << "; высота: " << height << "\n";
-	return ss.str();
+    stringstream ss;
+    ss << "Цилиндр с центром в точке (" << x << ", " << y <<
+       ") и радиусом " << radius << "; высота: " << height << "\n";
+    return ss.str();
 }
 
 double Cylinder::get_volume()
 {
-	return M_PI * radius * radius * height;
+    return M_PI * radius * radius * height;
+}
+
+void Cylinder::setRadius(const double radius)
+{
+    if(radius > 0) {
+        this->radius = radius;
+    } else {
+        cout << "Invalid radius value\n";
+    }
+}
+
+void Cylinder::setHeight(const double height) {
+    if(height > 0) {
+        this->height = height;
+    } else {
+        cout << "Invalid height value\n";
+    }
+}
+Cylinder::~Cylinder()
+{
+
 }
